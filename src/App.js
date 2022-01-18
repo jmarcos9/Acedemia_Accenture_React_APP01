8ee58e7e90184eff1cca72add8a500d4acdbbad0
jmarcos9 <66381477+jmarcos9@users.noreply.github.com>
2022-01-17 22:49:20 -0300

    Primeiro app React

---
+++ src/App.js
@@ -0,0 +1,26 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+function App() {
+  return (
+    <>
+      <form>
+        <input type="text" placeholder="Nome"></input>
+        <textarea placeholder="Fale sobre você"/>
+      </form>
+
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h2>Título</h2>
+            </header>
+            <p>Fale sobre você</p>
+          </section>
+        </li>
+      </ul>      
+    </>
+  );
+}
+
+export default App;
